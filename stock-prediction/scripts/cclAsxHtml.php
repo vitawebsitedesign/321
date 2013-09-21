10da47fd9672470880edb3fc9b67c181940bd6bc
Michael Nguyen <michael.nguyen5777@gmail.com>
2013-09-21 22:22:20 +1000

    clean

--- stock-prediction/scripts/cclAsxHtml.php
+++
@@ -1,7 +0,0 @@
-<?php
-	include_once('getHtml.php');
-	$cclHtml = file_get_contents('http://www.asx.com.au/asx/research/companyInfo.do?by=asxCode&asxCode=CCL');
-	$tagType = 'class';
-	$tag = 'datatable';
-	echo getDOMHtml($cclHtml, $tagType, $tag);
-?>
--- stock-prediction/scripts/cclPriceHistory.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	$.ajax({
-		url: 'scripts/cclAsxHtml.php',
-		type: 'post',
-		success: function(priceHistoryHtml) {
-			$(priceHistoryHtml).appendTo('#priceHistoryTables');	
-		}
-	});
-});
